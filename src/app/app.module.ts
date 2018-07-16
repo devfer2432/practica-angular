55430151d8eef260c75974a76d7a763a48d67648
ferkki <ferkki@hotmail.com>
2018-07-16 17:06:54 -0400

    practica 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UsuarioComponent } from './usuario/usuario.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundoComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule
